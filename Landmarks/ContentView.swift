39dfff6613905fc0cbece1308f34c21cc8927924
yushaojian <yushaojian13@163.com>
2019-12-29 10:05:25 +0800

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Shawn Yu on 12/29/19.
+//  Copyright © 2019 Yu Shaojian. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
